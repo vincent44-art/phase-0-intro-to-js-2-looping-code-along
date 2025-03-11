92072afd7346956a003351cdae85b87df27c8498
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-11 12:45:37 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// Function to create thank-you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a given number to zero
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example usage
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  countDown(10);
+  
+
+
